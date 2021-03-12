b91ec91fe9ac77cbc4e53fe36c0c5a64df7e3196
Bruno Stelzeneder <bruno.stelzeneder@gmail.com>
2021-03-12 15:21:17 -0300

    adicionado repositorio e busca por id e email dos usuarios

---
+++ hr-user/src/main/java/br/com/alura/hruser/repositories/UserRepository.java
@@ -0,0 +1,16 @@
+package br.com.alura.hruser.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.alura.hruser.entities.User;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	User findByEmail(String email);
+	
+	
+	
+}
