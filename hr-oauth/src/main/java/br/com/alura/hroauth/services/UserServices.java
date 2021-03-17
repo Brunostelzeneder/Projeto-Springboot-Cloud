0592e519104a954e9245c40fc350750ed4f04688
Bruno Stelzeneder <bruno.stelzeneder@gmail.com>
2021-03-17 17:37:02 -0300

    Adicionado JWT Token

--- hr-oauth/src/main/java/br/com/alura/hroauth/services/UserServices.java
+++ hr-oauth/src/main/java/br/com/alura/hroauth/services/UserServices.java
@@ -2,13 +2,16 @@ package br.com.alura.hroauth.services;
 
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import br.com.alura.hroauth.entities.User;
 import br.com.alura.hroauth.feignClients.UserFeignClients;
 
 @Service
-public class UserServices {
+public class UserServices implements UserDetailsService {
 
 	private static Logger logger = org.slf4j.LoggerFactory.getLogger(UserServices.class);
 	
@@ -21,7 +24,20 @@ public class UserServices {
 				logger.error("Email not found: " + email);
 				throw new IllegalArgumentException("Email not found");
 			}
-			logger.error("Email found: " + email);
+			logger.info("Email found: " + email);
 			return user;
 	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userFeignClients.findByEmail(username).getBody();
+		if (user == null) {
+			logger.error("Email not found: " + username);
+			throw new IllegalArgumentException("Email not found");
+		}
+		logger.info("Email found: " + username);
+		return user;
+}
+
+
 }
--- hr-user/src/main/java/br/com/alura/hruser/entities/User.java
+++ hr-user/src/main/java/br/com/alura/hruser/entities/User.java
@@ -15,34 +15,28 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
-
 @Entity
 @Table(name = "tb_user")
-public class User implements Serializable {
+public class User implements Serializable
+{
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-	
+
 	@Column(unique = true)
 	private String email;
 	private String password;
-	
+
 	@ManyToMany(fetch = FetchType.EAGER)
-	@JoinTable(
-			name = "tb_user_role",
-			joinColumns = @JoinColumn(name = "user_id"),
-			inverseJoinColumns = @JoinColumn(name = "role_id")
-			) 			
+	@JoinTable(name = "tb_user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
 	private Set<Role> roles = new HashSet<>();
-	
-	
-	public User() {}
 
-
+	public User() {
+	}
 
 	public User(Long id, String name, String email, String password) {
 		super();
@@ -52,10 +46,6 @@ public class User implements Serializable {
 		this.password = password;
 	}
 
-	
-	
-
-
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -63,8 +53,6 @@ public class User implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -83,60 +71,42 @@ public class User implements Serializable {
 		return true;
 	}
 
-
-
 	public Long getId() {
 		return id;
 	}
-
-
 
 	public void setId(Long id) {
 		this.id = id;
 	}
 
-
-
 	public String getName() {
 		return name;
 	}
-
-
 
 	public void setName(String name) {
 		this.name = name;
 	}
 
-
-
 	public String getEmail() {
 		return email;
 	}
-
-
 
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
-
-
 	public String getPassword() {
 		return password;
 	}
-
-
 
 	public void setPassword(String password) {
 		this.password = password;
 	}
 
-
-
 	public Set<Role> getRoles() {
 		return roles;
 	}
-	
-	
+
+
 
 }
